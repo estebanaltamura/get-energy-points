95c7a8db16ff8e69546fa42d43224612841dbdc7
Andres <esteban.altamura@gmail.com>
2024-08-09 10:55:31 +0200

    update

--- index.js
+++ index.js
@@ -7,20 +7,27 @@ import fs from 'fs';
 const app = express();
 app.use(cors());
 
+let isProcessing = false; // Variable de bloqueo
+
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 
 app.get('/scrape', async (req, res) => {
+  if (isProcessing) {
+    return res.status(429).send('Server is busy processing another request. Please try again later.');
+  }
+
+  isProcessing = true; // Indicar que se ha iniciado un procesamiento
   let browser;
   try {
-    // Cambia la ruta del ejecutable para usar Google Chrome
+    // Iniciar Puppeteer con Google Chrome
     browser = await puppeteer.launch({
       headless: true,
       executablePath: '/usr/bin/google-chrome', // Ruta para Google Chrome
       args: ['--no-sandbox', '--disable-setuid-sandbox'], // Opciones recomendadas para servidores
     });
-    
+
     const page = await browser.newPage();
     await page.goto('https://www.khanacademy.org/profile/idev0x00', { waitUntil: 'networkidle0' });
     const content = await page.content();
@@ -29,6 +36,8 @@ app.get('/scrape', async (req, res) => {
   } catch (error) {
     console.error('Error fetching the page:', error);
     res.status(500).send('Error fetching the page');
+  } finally {
+    isProcessing = false; // Liberar el bloqueo cuando se completa el procesamiento
   }
 });
 
