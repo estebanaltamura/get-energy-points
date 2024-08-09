80fb4e67d3b1afa77d265e189ce5d93ed7e070dd
Andres <esteban.altamura@gmail.com>
2024-08-09 10:44:12 +0200

    update

--- index.js
+++ index.js
@@ -14,18 +14,39 @@ app.get('/health', (req, res) => {
 app.get('/scrape', async (req, res) => {
   let browser;
   try {
-    // Cambia la ruta del ejecutable para usar Google Chrome
+    // Obtener la clase y la URL desde la consulta
+    const { className, url } = req.query;
+
+    if (!className || !url) {
+      return res.status(400).send('Error: both className and url parameters are required');
+    }
+
     browser = await puppeteer.launch({
       headless: true,
-      executablePath: '/usr/bin/google-chrome', // Ruta para Google Chrome
-      args: ['--no-sandbox', '--disable-setuid-sandbox'], // Opciones recomendadas para servidores
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      executablePath: '/usr/bin/headless-chromium', // Ruta al ejecutable de Chromium
     });
-    
+
     const page = await browser.newPage();
-    await page.goto('https://www.khanacademy.org/profile/idev0x00', { waitUntil: 'networkidle0' });
+    await page.goto(url, { waitUntil: 'networkidle0' });
     const content = await page.content();
     await browser.close();
-    res.status(200).send(content);
+
+    // Crear una expresión regular dinámica usando la clase proporcionada
+    const regex = new RegExp(`class="${className}"[^>]*>([^<]*)<`, 'g');
+    let match;
+    const matches = [];
+
+    // Buscar todas las coincidencias en el contenido de la página
+    while ((match = regex.exec(content)) !== null) {
+      matches.push(match[1].trim());
+    }
+
+    if (matches.length > 0) {
+      res.status(200).send(matches);
+    } else {
+      res.status(404).send('Class not found or no content available');
+    }
   } catch (error) {
     console.error('Error fetching the page:', error);
     res.status(500).send('Error fetching the page');
@@ -43,4 +64,3 @@ const port = 3200;
 server.listen(port, () => {
   console.log(`Servidor HTTPS escuchando en el puerto ${port}`);
 });
-
